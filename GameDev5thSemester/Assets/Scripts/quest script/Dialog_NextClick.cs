ddfa6f5241587b0694ae83d2c191b4b543d0e727
Simon <shurygin.sa@students.dvfu.ru>
2020-10-07 16:22:48 +1000

    add mini quest

---
+++ GameDev5thSemester/Assets/Scripts/quest script/Dialog_NextClick.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog_NextClick : MonoBehaviour {
+	public GameObject Text1;
+	public GameObject Text2;
+	private bool isText1 =true;
+	public NPC_Task npc_taskScript;
+    public bool Fin_Dialog;
+    public GameObject ObjectQuest;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (isText1 == true) {
+				isText1 = false;
+			}
+			else{
+                if (Fin_Dialog == false)
+                {
+                    isText1 = true;
+                    ObjectQuest.SetActive(true);
+                    npc_taskScript.EndDialog = true;
+                }
+                else
+                {
+                    isText1 = true;
+                    npc_taskScript.Fin_Dialog = true;
+                }
+			}
+		}
+		if (isText1 == true) {
+			Text1.SetActive (true);
+			Text2.SetActive (false);
+		} else {
+			Text1.SetActive (false);
+			Text2.SetActive (true);
+		}
+	}
+}
---
+++ GameDev5thSemester/Assets/Scripts/quest script/NPC_Task.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Task : MonoBehaviour {
+	public bool EndDialog;
+	public GameObject Dialog1, Dialog2, joystic;
+	public Quest_Event QE;
+    public bool Fin_Dialog;
+    private bool onhide;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (QE.end_Quest1 == true)
+        {
+            Fin_Dialog = true;
+        }
+		if (EndDialog == true) {
+			Time.timeScale = 1;
+			QE.Quest1 = true;
+			Dialog1.SetActive (false);
+            joystic.SetActive (true);
+        }
+        if (Fin_Dialog == true)
+        {
+            Time.timeScale = 1;
+            QE.Quest1 = false;
+            Dialog1.SetActive(false);
+            joystic.SetActive(true);
+        }
+	}
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.tag == "Player") {
+            if (Fin_Dialog == false)
+            {
+                Time.timeScale = 0;
+                if (QE.end_Quest1 == false)
+                {
+                    Dialog1.SetActive(true);
+                    joystic.SetActive(false);
+                }
+                else
+                {
+                    Dialog2.SetActive(true);
+                    joystic.SetActive(false);
+                }
+            }
+            else
+            {
+                if (onhide == false)
+                {
+                    Dialog2.SetActive(true);
+                    onhide = true;
+                }
+            }
+        }
+	}
+}

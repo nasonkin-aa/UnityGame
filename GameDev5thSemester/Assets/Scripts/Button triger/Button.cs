755ed6805953aeb36e0d5f2cfca69f801fdcca27
Andrey <nasonkin-andrey@mail.ru>
2020-11-11 21:43:24 +1000

    Add button trigger
    If you click on the button in trigger  console will write 1 out of  trigger will write 0

---
+++ GameDev5thSemester/Assets/Scripts/Button triger/Button.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    ButtonTrigger ButtonTrigger;
+    public GameObject obj;
+    private void Start()
+    {
+        ButtonTrigger = obj.GetComponent<ButtonTrigger>();
+    }
+    private void OnMouseDown()
+    {
+        if (ButtonTrigger.ButtonInTrigger == true)
+        {
+            Debug.Log("1");
+        }
+        else if (ButtonTrigger.ButtonInTrigger == false)
+        {
+            Debug.Log("0");
+        }
+        
+    }
+}
---
+++ GameDev5thSemester/Assets/Scripts/Button triger/ButtonTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger: MonoBehaviour
+{
+    public GameObject frame;
+    public GameObject[] OtheFrame;
+    public bool ButtonInTrigger =false;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("OH SHIT A RAT");
+            frame.SetActive(true);
+            ButtonInTrigger = true;
+            foreach (GameObject frame in OtheFrame)
+            {
+                frame.SetActive(false);
+            }
+
+        }
+        
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("lol");
+            ButtonInTrigger = false;
+
+        }
+    }
+}

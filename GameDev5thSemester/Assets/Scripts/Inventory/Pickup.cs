e829050e13e68b668be0e9a298e06f7f4371b8aa
Andrey <nasonkin-andrey@mail.ru>
2020-12-01 19:19:54 +1000

    Add script pickup

---
+++ GameDev5thSemester/Assets/Scripts/Inventory/Pickup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject SlotButton;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for( int i = 0; i < inventory.slots.Length; i++)
+            {
+                if( inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    Instantiate(SlotButton, inventory.slots[i].transform);
+                    Destroy(gameObject);
+                    break;
+
+                }
+            }
+
+
+        }
+    }
+}

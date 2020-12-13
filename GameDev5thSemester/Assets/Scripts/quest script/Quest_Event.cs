7ab0bbc869227c4e19da3062b084b84b6d7e6388
Simon <shurygin.sa@students.dvfu.ru>
2020-12-13 21:04:45 +1000

    Add SubQuest with Quest 2

--- GameDev5thSemester/Assets/Scripts/quest script/Quest_Event.cs
+++ GameDev5thSemester/Assets/Scripts/quest script/Quest_Event.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Quest_Event : MonoBehaviour {
-	public bool Quest1, Quest2;
-	public GameObject Quest1t, Quest2t;
-    public bool end_Quest1, end_Quest2;
+	public bool Quest1, Quest2, SubQuest2;
+	public GameObject Quest1t, Quest2t, SubQuest2t;
+    public bool end_Quest1, end_Quest2, end_SubQuest2;
     public int n = 0;
 
 	// Use this for initialization
@@ -30,7 +30,7 @@ public class Quest_Event : MonoBehaviour {
         {
             Quest1t.SetActive(false);
         }
-
+        //
         if (end_Quest2 == false)
         {
             if (Quest2 == true)
@@ -45,6 +45,22 @@ public class Quest_Event : MonoBehaviour {
         else
         {
             Quest2t.SetActive(false);
+        }
+        //
+        if (end_SubQuest2 == false)
+        {
+            if (SubQuest2 == true)
+            {
+                SubQuest2t.SetActive(true);
+            }
+            else
+            {
+                SubQuest2t.SetActive(false);
+            }
+        }
+        else
+        {
+            SubQuest2t.SetActive(false);
         }
 
     }

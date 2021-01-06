3b65b2f8627485332dd489e84a36d2e873b0d3a0
Andrey <nasonkin-andrey@mail.ru>
2021-01-06 23:20:28 +1000

    Add script NumButton

---
+++ GameDev5thSemester/Assets/Scripts/MiniGame/Numbers/NumButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumButton : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        
+    }
+}

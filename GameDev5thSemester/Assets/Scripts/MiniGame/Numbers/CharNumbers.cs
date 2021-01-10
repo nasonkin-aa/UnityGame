060e6bef380b2ce4cf673e1f7e7fec4edd55ad85
Andrey <nasonkin-andrey@mail.ru>
2021-01-11 00:37:29 +1000

    Fix script CharNumbers

--- GameDev5thSemester/Assets/Scripts/MiniGame/Numbers/CharNumbers.cs
+++ GameDev5thSemester/Assets/Scripts/MiniGame/Numbers/CharNumbers.cs
@@ -5,26 +5,32 @@ using UnityEngine;
 public class CharNumbers : MonoBehaviour
 {
     public GameObject[] Projectiles;
-
+    public sbyte Num;
+  
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+       
+        if (Num == 1)
         {
             Instantiate(Projectiles[0], transform.position, Quaternion.identity);
+            Debug.Log("ok");
+            Num = 0;
 
         }
-        else if (Input.GetKeyDown(KeyCode.Z))
+        else if (Num == 2)
         {
             Instantiate(Projectiles[1], transform.position, Quaternion.identity);
-
+            Num = 0;
 
         }
-        else if (Input.GetKeyDown(KeyCode.E))
+        else if (Num == 3)
         {
             Instantiate(Projectiles[2], transform.position, Quaternion.identity);
+            Num = 0;
 
 
         }
     }
+    
 
 }

c7f290fbd08c071802f4a06c356d278bee5de2f2
Andrey <nasonkin-andrey@mail.ru>
2021-01-11 00:36:32 +1000

    Add script NumButtonShot

---
+++ GameDev5thSemester/Assets/Scripts/MiniGame/Numbers/NumButtonShot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumButtonShot : MonoBehaviour
+{
+    public CharNumbers charNumbers;
+   
+    public void OnMouseDown()
+    
+    {
+        switch (gameObject.name)
+        {
+            case "B1":
+                {
+                    charNumbers.Num = 1;
+                    
+
+                }
+                break;
+            case "B2":
+                {
+                    charNumbers.Num = 2;
+                    
+                }
+                break;
+            case "B3":
+                {
+
+                    charNumbers.Num = 3;
+                }
+                break;
+        }
+
+    }
+}

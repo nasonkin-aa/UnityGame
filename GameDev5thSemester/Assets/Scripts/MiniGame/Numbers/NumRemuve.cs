b6588973aa974390d9d5a31cadd4519ba2f3887c
Andrey <nasonkin-andrey@mail.ru>
2021-01-12 02:54:30 +1000

    Fix Minigame -Numbers

---
+++ GameDev5thSemester/Assets/Scripts/MiniGame/Numbers/NumRemuve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumRemuve : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag != gameObject.tag)
+        {
+
+            Destroy(GameObject.FindGameObjectWithTag("MGBFirst"));
+            Destroy(GameObject.FindGameObjectWithTag("MGBSecond"));
+            Destroy(GameObject.FindGameObjectWithTag("MGBThird"));
+           
+        }
+
+    }
+}

4132b1e5914f4acf26cb1a52332cfb5beb2c08dc
ika <aleks.5@inbox.ru>
2021-01-14 18:07:09 +1000

    Edit scene

---
+++ GameDev5thSemester/Assets/Scripts/quest script/trash/Quest_Button.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest_Button : MonoBehaviour
+{
+
+    public Animator AnimMiniGaem;
+    public GameObject controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = GameObject.Find("Controller");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        if (PlayerPrefs.GetInt ("questClicker") == 1)
+        {
+            PlayerPrefs.SetInt ("questClicker", 2);
+        }
+        controller.SetActive(true);
+        AnimMiniGaem.SetTrigger("IsTriggered");
+    }
+}

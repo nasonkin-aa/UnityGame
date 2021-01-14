1769f31e94ea99b47753284de793fe89af24f04b
Simon <shurygin.sa@students.dvfu.ru>
2021-01-14 12:16:28 +1000

    Add geography test and geography teacher

---
+++ GameDev5thSemester/Assets/Scripts/quest script/trash/NPC_Spawn.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Spawn : MonoBehaviour
+{
+    public GameObject[] Npc;
+    private int rnd;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rnd = Random.Range(0, Npc.Length);
+        Instantiate(Npc[rnd], transform.position, Quaternion.identity);
+    }
+}
---
+++ GameDev5thSemester/Assets/Scripts/quest script/trash/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score :MonoBehaviour
+{
+
+    public static int scre;
+    public static GUIStyle razmer;
+
+    void Start()
+    {
+        razmer = new GUIStyle();
+
+        razmer.fontSize = 30;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(1030, 40, 100, 100), "Score: " + scre, razmer);
+    }
+
+    static void Update()
+    {
+        
+    }
+}

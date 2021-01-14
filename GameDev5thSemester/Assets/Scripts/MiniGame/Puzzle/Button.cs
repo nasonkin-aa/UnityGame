ac2b5aa22018815b881b85d741720c42fc9fa155
ika <aleks.5@inbox.ru>
2021-01-14 16:37:00 +1000

    Add miniGame Puzzle

---
+++ GameDev5thSemester/Assets/Scripts/MiniGame/Puzzle/Button.cs
@@ -0,0 +1,18 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public string action;
+
+    void OnMouseUpAsButton()
+    {
+        switch (gameObject.name)
+        {
+            case "PLay":
+                Application.LoadLevel("play");
+                break;
+        }
+    }
+}*/

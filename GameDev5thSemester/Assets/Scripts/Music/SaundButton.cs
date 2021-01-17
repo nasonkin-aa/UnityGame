8ae7be4db604ca1353b6b90b35c8a7b1efbb7407
Andrey <nasonkin-andrey@mail.ru>
2021-01-17 18:23:50 +1000

    Fix minigame  -Numbers

---
+++ GameDev5thSemester/Assets/Scripts/Music/SaundButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaundButton : MonoBehaviour
+{
+    public AudioSource MySound;
+    public AudioClip clikSou;
+    
+    public void ClickSound()
+    {
+        MySound.PlayOneShot(clikSou);
+    }
+}
